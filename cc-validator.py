5a7002dfee70b3a9d928468bf4587a0936f03f4b
AgSpades <86034982+AgSpades@users.noreply.github.com>
2024-06-08 23:10:39 +0530

    Define Luhn algorithm logic.

--- cc-validator.py
+++ cc-validator.py
@@ -1,17 +1,29 @@
+
 def main():
-    card_number = input("Enter credit card number: ") 
-    if check_valid_card(card_number):
+    card_number = input("Enter credit card number: ").strip()
+    if is_valid_card(card_number):
         print("Valid credit card number.")
     else:
         print("Invalid credit card number.")
 
-def check_valid_card(n):
-    if not n.isdigit():
-        return False
-    return luhn_check(n)
+def luhn_check(card_number):
+    def digits_of(n):
+        return [int(d) for d in str(n)]
+    
+    digits = digits_of(card_number)
+    odd_digits = digits[-1::-2]
+    even_digits = digits[-2::-2]
+    checksum = sum(odd_digits)
+    
+    for d in even_digits:
+        checksum += sum(digits_of(d * 2))
+        
+    return checksum % 10 == 0
 
-def luhn_check(n):
-    pass
+def is_valid_card(card_number):
+    if not card_number.isdigit() or len(card_number) != 16:
+        return False
+    return luhn_check(card_number)
 
 if __name__ == "__main__":
     main()
